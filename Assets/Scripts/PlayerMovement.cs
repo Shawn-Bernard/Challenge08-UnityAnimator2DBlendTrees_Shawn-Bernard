fac0e797bc54e2f9f579bb291ae3acba726acab4
Shawn Bernard <ssbernard@outlook.com>
2025-03-03 21:04:41 -0400

    Added idle animation blend tree

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,9 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D playerRigidbody;
     Animator animator;
     //My player 
-    Vector2 playerVector; 
+    Vector2 playerVector;
+
+    Vector2 lastPlayerVector;
     //Speed so we can go fast
     public float Speed = 2.0f;
     private void Awake()
@@ -18,10 +20,6 @@ public class PlayerMovement : MonoBehaviour
 
         //adding my update move vector mwthod to my MoveEvent action
         PlayerInputActions.MoveEvent += UpdateMoveVector;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
     }
 
     // Update is called once per frame
@@ -35,11 +33,15 @@ public class PlayerMovement : MonoBehaviour
         //Moving my rigidbody2d player position getting my rigidbody position and adding my player vector 
         playerRigidbody.MovePosition(playerRigidbody.position + playerVector  * Time.fixedDeltaTime * Speed);
     }
-    void UpdateMoveVector(Vector2 inputMovement)
+    void UpdateMoveVector(Vector2 inputVector)
     {
-        //Making my moveVector equal to Movement 
-        playerVector = inputMovement;
+        //Setting my last position to the player vector before I change player vector
+        lastPlayerVector = playerVector;
 
+        //Making my moveVector equal to input vector
+        playerVector = inputVector;
+
+        //Calling my player animation method after
         PlayerAnimation();
     }
     void PlayerAnimation()
@@ -47,16 +49,23 @@ public class PlayerMovement : MonoBehaviour
         //If my player vector is vector (0,0), set my moving bool to false 
         if (playerVector == Vector2.zero)
         {
-            animator.SetBool("Moving", false);
+            //Throwing in my last player vector into my x and y when not moving 
+            SetAnimatorValues(lastPlayerVector,false);
         }
         else
         {
             //If my player vector isn't vector zero, set moving to true
-            animator.SetBool("Moving", true);
             //Setting my floats X & Y to player x and y position
-            animator.SetFloat("X", playerVector.x);
-            animator.SetFloat("Y", playerVector.y);
+            SetAnimatorValues(playerVector,true);
         }
+    }
+    //Takes in a vector for the last player vector and new player vector
+    void SetAnimatorValues(Vector2 playerVector,bool Bool)
+    {
+        //Player vector.x or y will give a float instead of a vector
+        animator.SetFloat("X", playerVector.x);
+        animator.SetFloat("Y", playerVector.y);
+        animator.SetBool("Moving", Bool);
     }
     private void OnDisable()
     {
